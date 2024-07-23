2ffc61922caa6ca80ba3873c077b0ac989e62796
DARSHANA0206 <darshanapriya0206@gmail.com>
2024-07-23 20:49:17 +0530

    2.py

--- TASK 2/2.py
+++ TASK 2/2.py
@@ -1,36 +1,35 @@
-# PERFORM SIMPLE DATA CLEANING TASKS SUCH AS HANDLING MISSING VALUES AND REMOVING DUPLICATES.
 import pandas as p
 
 data = p.read_csv("01.Data Cleaning and Preprocessing.csv")
 
-print("\n**********DUPLICATE**********\n")
+print("\nDUPLICATE\n")
 data1 = data.drop_duplicates()
 print(data1)
 
-print("\n**********NULL**********\n")
+print("\nNULL\n")
 data2 = data.isnull()
 print(data2)
 
-print("\n**********NULL.SUM**********\n")
+print("\nNULL.SUM\n")
 data3 = data.isnull().sum()
 print(data3)
 
-print("\n**********NOTNULL**********\n")
+print("\nNOTNULL\n")
 data4 = data.notnull()
 print(data4)
 
-print("\n**********NULL.SUM.SUM**********\n")
+print("\nNULL.SUM.SUM\n")
 data5 = data.isnull().sum().sum()
 print(data5)
 
-print("\n**********HANDLING METHOD 1**********\n")
+print("\nHANDLING METHOD 1\n")
 data6 = data.fillna(value = 0)
 print(data6)
 
-print("\n**********HANDLING METHOD 2**********\n")
+print("\nHANDLING METHOD 2\n")
 data7 = data.fillna(method = "pad")
 print(data7)
 
-print("\n**********HANDLING METHOD 3**********\n")
+print("\nHANDLING METHOD 3\n")
 data8 = data.fillna(method = "bfill")
 print(data8)
