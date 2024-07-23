82e4a3c6c498a47c007bf6fa1153ef56944bb37c
DARSHANA0206 <darshanapriya0206@gmail.com>
2024-07-23 20:47:35 +0530

    1.py

--- TASK 2/1.py
+++ TASK 2/1.py
@@ -1,14 +1,12 @@
-# LEARN HOW TO READ CSV FILES AND MANIPULATE DATA FRAMES USING PANDAS.
-
 import pandas as p
 
 data = p.read_csv("01.Data Cleaning and Preprocessing.csv")
 
-print("\n**********TYPE**********\n")
+print("\nTYPE\n")
 print(type(data))
 
-print("\n**********INFO**********\n")
+print("\nINFO\n")
 print(data.info())
 
-print("\n**********DESCRIBE**********\n")
+print("\nDESCRIBE\n")
 print(data.describe())
